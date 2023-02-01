9e9d1eed190e0d2ada9612a6ceb23acc4e0140d4
Eliton da Rosa <elitondr@live.com>
2023-01-31 21:56:58 -0300

    Detalhes do Produto finalizado

--- src/components/AvaliationProduct.jsx
+++ src/components/AvaliationProduct.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/AvaliationProduct.sass';
+import { Rating } from 'react-simple-star-rating';
 
 export default class AvaliationProduct extends Component {
   render() {
@@ -12,15 +13,18 @@ export default class AvaliationProduct extends Component {
             <p data-testid="review-card-email" className="avaliation-email">
               { avaliation.email }
             </p>
-            <p data-testid="review-card-rating" className="avaliation-rating">
-              { avaliation.rating }
-            </p>
+            <Rating
+              className="avaliation-rating"
+              initialValue={ avaliation.rating }
+              readonly
+              size={ 30 }
+
+            />
             <p data-testid="review-card-evaluation" className="avaliation-text">
               { avaliation.text }
             </p>
           </div>
         ))}
-        ;
       </div>
     );
   }
