ec9bf6a081e523f4020877642833be5f145a3369
Eliton da Rosa <elitondr@live.com>
2023-02-01 01:39:10 -0300

    Fix: CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,25 @@ class Header extends Component {
     const { handleChange, onClickButton } = this.props;
     return (
       <div className="container-header">
-        <Search
-          handleChange={ handleChange }
-          onClickButton={ onClickButton }
-        />
-        <Link to="/">
-          <img src={ logo } alt="Logo" className="logo" />
-        </Link>
-        <Link
-          to="/carrinho"
-          data-testid="shopping-cart-button"
-        >
-          <img src={ cart } alt="Carrinho de compras" />
-        </Link>
+        <div className="logo-div">
+          <Link to="/">
+            <img src={ logo } alt="Logo" className="logo" />
+          </Link>
+        </div>
+        <div className="search-div">
+          <Search
+            handleChange={ handleChange }
+            onClickButton={ onClickButton }
+          />
+        </div>
+        <div className="cart-link">
+          <Link
+            to="/carrinho"
+            data-testid="shopping-cart-button"
+          >
+            <img src={ cart } alt="Carrinho de compras" />
+          </Link>
+        </div>
       </div>
     );
   }
